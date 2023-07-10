3988543f8c5e1651673be577150cf0b61e86a204
Mishacreatrix <michellemcc76@gmail.com>
2023-07-10 12:35:57 +0100

    20230710 adding more gens and filters

--- app.js
+++ app.js
@@ -1,28 +1,55 @@
 const pokedex = document.getElementById("pokedex");
+let pokemon = [];
+let filteredPokemon = [];
 
 const fetchPokemon = () => {
-  const promises = [];
-  for (let i = 1; i <= 151; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    promises.push(fetch(url).then((res) => res.json()));
-  }
-  Promise.all(promises).then((results) => {
-    const pokemonPromises = results.map((result) => {
-      const generationUrl = result.species.url;
-      return fetch(generationUrl).then((res) => res.json());
-    });
+  const generationUrl = "https://pokeapi.co/api/v2/generation";
 
-    Promise.all(pokemonPromises).then((generationResults) => {
-      const pokemon = results.map((result, index) => ({
-        name: result.name,
-        image: result.sprites["front_default"],
-        type: result.types.map((type) => type.type.name).join(", "),
-        id: result.id,
-        generation: generationResults[index].generation.name.substring(11),
-      }));
-      displayPokemon(pokemon);
+  fetch(generationUrl)
+    .then((res) => res.json())
+    .then((generationData) => {
+      const generations = generationData.results.slice(0, 4);
+      const generationButtons = generations.map((generation) => {
+        const generationName = generation.name.substring(11);
+        return `<button class="filter-button bg-violet-500 hover:bg-violet-600 px-4 py-2 rounded-md font-medium text-white shadow-sm text-sm transition-all uppercase" data-generation="${generationName}">GEN ${generationName}</button>`;
+      });
+
+      const filterButtonsContainer = document.getElementById("filter-buttons");
+      filterButtonsContainer.innerHTML = generationButtons.join("");
+
+      addFilterListeners();
+
+      const promises = [];
+      for (let i = 1; i <= 493; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+      }
+      Promise.all(promises).then((results) => {
+        const pokemonPromises = results.map((result) => {
+          const generationUrl = result.species.url;
+          return fetch(generationUrl).then((res) => res.json());
+        });
+
+        Promise.all(pokemonPromises).then((generationResults) => {
+          pokemon = results.map((result, index) => ({
+            name: result.name,
+            image: result.sprites["front_default"],
+            type: result.types.map((type) => type.type.name).join(", "),
+            id: result.id,
+            generation: generationResults[index].generation.name.substring(11),
+          }));
+          filteredPokemon = [...pokemon]; // Set filteredPokemon to complete list initially
+          displayPokemon(filteredPokemon);
+        });
+      });
     });
-  });
+};
+
+const filterPokemonByGeneration = (generation) => {
+  filteredPokemon = pokemon.filter(
+    (pokeman) => pokeman.generation === generation
+  );
+  displayPokemon(filteredPokemon);
 };
 
 const displayPokemon = (pokemon) => {
@@ -41,6 +68,17 @@ const displayPokemon = (pokemon) => {
     )
     .join("");
   pokedex.innerHTML = pokemonHTMLString;
+};
+
+const addFilterListeners = () => {
+  const filterButtons = document.querySelectorAll(".filter-button");
+
+  filterButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const selectedGeneration = button.dataset.generation;
+      filterPokemonByGeneration(selectedGeneration);
+    });
+  });
 };
 
 fetchPokemon();
